e79a37035b6db57cba472003136e9bbc68f96693
Dias <wprogrammist@gmail.com>
2024-09-11 06:19:12 +0500

    import command

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 const vscode = require('vscode');
 const ncp = require('copy-paste');
 let HTMLParser = require('node-html-parser');
-const { querySelectorCommand } = require('./src/commands');
+const { querySelectorCommand, querySelectorAllCommand } = require('./src/commands');
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -29,7 +29,7 @@ function activate(context) {
   );
 
   context.subscriptions.push(
-    vscode.commands.registerCommand('selector-js.qsa', () => {}),
+    vscode.commands.registerCommand('selector-js.qsa', querySelectorAllCommand),
   );
 
   context.subscriptions.push(
