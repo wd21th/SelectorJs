8509c489805c2d250f202a94a99f40cbe3017339
Dias <wprogrammist@gmail.com>
2024-09-21 00:21:32 +0500

    refactor function

--- src/utils/set-attributes.js
+++ src/utils/set-attributes.js
@@ -2,15 +2,12 @@
  * Description
  * @param {any} attrs
  * @param {any} attrsObj
- * @returns {any}
+ * @returns { { [key: string]: string } }
  */
 function setAttributes (attrs, attrsObj) {
-  for (let i = 0; i < attrs.match(/([a-zA-Z\d-_]+)(?==)/g).length; i++) {
-    let attributeName = attrs.match(/([a-zA-Z\d-_]+)(?==)/g)[i],
-      attributeValue = attrs.match(/".+?"/g)[i];
-
-    attrsObj[attributeName] = attributeValue;
-  }
+  return attrs.match(/([a-zA-Z\d-_]+)(?==)/g).reduce((acc, attributeName, index) => {
+    acc[attributeName] = attrs.match(/".+?"/g)[index]
+  }, {})
 }
 
 module.exports = setAttributes;
