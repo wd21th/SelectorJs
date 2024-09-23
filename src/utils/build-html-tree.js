48a20922cdf3c86ef2626c6767b2808c3af7d201
Dias <wprogrammist@gmail.com>
2024-09-23 20:41:03 +0500

    add description for function

--- src/utils/build-html-tree.js
+++ src/utils/build-html-tree.js
@@ -4,7 +4,11 @@ const setAttributes = require("./set-attributes");
 let htmlParser = require('node-html-parser');
 
 /**
- * Description
+ * Recursively processes an HTML element and its children, building a nested representation of the elements.
+ * 
+ * This function parses an HTML element and its attributes, recursively traverses its child nodes, and constructs 
+ * a nested array of custom `htmlElement` objects representing the structure of the HTML tree. Each element 
+ * includes details such as the tag name, attributes, nesting level, and parent element.
  * @param {htmlParser.HTMLElement} htmlEl
  * @param {Array<htmlElement>} arr
  * @param {number} nestingLevel
@@ -28,11 +32,11 @@ function buildHtmlTree (htmlEl, arr = [], nestingLevel = 0, parent = null) {
     arr.push(tag);
     let parentEl = new parentElement(tagName, attrsObj);
     nestingLevel++;
-    
+
     const map = htmlEl.childNodes.map((el, i) => {
       return buildHtmlTree(htmlEl.childNodes[i], [...arr], nestingLevel, parentEl);
     })
-    
+
     return map;
   }
 }
