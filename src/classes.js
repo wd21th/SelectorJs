3fe36250f868c053d420e255b0b59a98ec6c0ddc
Dias <wprogrammist@gmail.com>
2024-09-23 20:42:30 +0500

    add description for classes

--- src/classes.js
+++ src/classes.js
@@ -1,5 +1,10 @@
 /**
- * Description
+ * Represents a parent HTML element in the tree structure.
+ * 
+ * This class is used to store the basic details of a parent HTML element, 
+ * including its tag name and attributes. It's useful for keeping track 
+ * of hierarchical relationships between HTML elements during tree traversal.
+ * 
  * @param {string} tagName
  * @param {string} attrsObj
  */
@@ -11,7 +16,12 @@ class parentElement {
 }
 
 /**
- * Description
+ * Represents an HTML element with detailed information about its structure.
+ * 
+ * This class is used to store various properties of an HTML element, including 
+ * its tag name, attributes, nesting level (depth in the DOM tree), and parent element.
+ * It's used to build a hierarchical representation of an HTML document.
+ * 
  * @param {string} tagName
  * @param {string} tabSize
  * @param { { [key: string]: any } } attrsObj
