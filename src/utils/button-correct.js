3b951b9e492c7276ad30676d6ec43f36f4d67a24
Dias <wprogrammist@gmail.com>
2024-10-03 22:34:45 +0500

    move to function

--- src/utils/button-correct.js
+++ src/utils/button-correct.js
@@ -1,6 +1,7 @@
 const { htmlElement } = require('../classes');
 const { allDoubleQuotes, anyFirstSymbol, emptySpace } = require('../regex');
 const checkIdVarName = require('./check-id-var-name');
+const upCase = require('./up-case');
 
 /**
  * Consist varable name from button attribute, uses querySelector method
@@ -14,25 +15,25 @@ function buttonCorrect(item) {
     let varableName = checkIdVarName(item.attrs['id']);
     let buttonId = item.attrs['id'].replace(allDoubleQuotes, emptySpace);
 
-    let varable = `button${varableName.replace(anyFirstSymbol, (match) => match.toUpperCase())} = document.querySelector('${item.tagName}#${buttonId}')`;
+    let varable = `button${varableName.replace(anyFirstSymbol, upCase)} = document.querySelector('${item.tagName}#${buttonId}')`;
     arr.push(varable);
   } else if (keys.includes('type')) {
     let varableName = checkIdVarName(item.attrs['type']);
     let buttonType = item.attrs['type'];
 
-    let varable = `button${varableName.replace(anyFirstSymbol, (match) => match.toUpperCase())} = document.querySelector('${item.tagName}[type=${buttonType}]')`;
+    let varable = `button${varableName.replace(anyFirstSymbol, upCase)} = document.querySelector('${item.tagName}[type=${buttonType}]')`;
     arr.push(varable);
   } else if (keys.includes('value')) {
     let varableName = checkIdVarName(item.attrs['value']);
     let buttonValue = item.attrs['value'];
 
-    let varable = `button${varableName.replace(anyFirstSymbol, (match) => match.toUpperCase())} = document.querySelector('${item.tagName}[value=${buttonValue}]')`;
+    let varable = `button${varableName.replace(anyFirstSymbol, upCase)} = document.querySelector('${item.tagName}[value=${buttonValue}]')`;
     arr.push(varable);
   } else if (keys.includes('name')) {
     let varableName = checkIdVarName(item.attrs['name']);
     let buttonName = item.attrs['name'];
 
-    let varable = `button${varableName.replace(anyFirstSymbol, (match) => match.toUpperCase())} = document.querySelector('${item.tagName}[name=${buttonName}]')`;
+    let varable = `button${varableName.replace(anyFirstSymbol, upCase)} = document.querySelector('${item.tagName}[name=${buttonName}]')`;
     arr.push(varable);
   } else {
     let varable = `button = document.querySelector('${item.tagName}')`;
---
+++ src/utils/up-case.js
@@ -0,0 +1,8 @@
+/**
+ * transform to upper case mathed string
+ * @param { string } match
+ * @returns { string }
+ */
+const upCase = (match) = match.toUpperCase();
+
+module.exports = upCase;
