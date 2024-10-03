6e84e566fea1444a681f3fbe03db38b12dcd3a3f
Dias <wprogrammist@gmail.com>
2024-10-03 22:14:05 +0500

    transform first case up

--- src/utils/button-correct.js
+++ src/utils/button-correct.js
@@ -14,25 +14,25 @@ function buttonCorrect(item) {
     let varableName = checkIdVarName(item.attrs['id']);
     let buttonId = item.attrs['id'].replace(allDoubleQuotes, emptySpace);
 
-    let varable = `button${varableName} = document.querySelector('${item.tagName}#${buttonId}')`;
+    let varable = `button${varableName.replace(/^./, (match) => match.toUpperCase())} = document.querySelector('${item.tagName}#${buttonId}')`;
     arr.push(varable);
   } else if (keys.includes('type')) {
     let varableName = checkIdVarName(item.attrs['type']);
     let buttonType = item.attrs['type'];
 
-    let varable = `button${varableName} = document.querySelector('${item.tagName}[type="${buttonType}"]')`;
+    let varable = `button${varableName.replace(/^./, (match) => match.toUpperCase())} = document.querySelector('${item.tagName}[type=${buttonType}]')`;
     arr.push(varable);
   } else if (keys.includes('value')) {
     let varableName = checkIdVarName(item.attrs['value']);
     let buttonValue = item.attrs['value'];
 
-    let varable = `button${varableName} = document.querySelector('${item.tagName}[value="${buttonValue}"]')`;
+    let varable = `button${varableName.replace(/^./, (match) => match.toUpperCase())} = document.querySelector('${item.tagName}[value=${buttonValue}]')`;
     arr.push(varable);
   } else if (keys.includes('name')) {
     let varableName = checkIdVarName(item.attrs['name']);
     let buttonName = item.attrs['name'];
 
-    let varable = `button${varableName} = document.querySelector('${item.tagName}[name="${buttonName}"]')`;
+    let varable = `button${varableName.replace(/^./, (match) => match.toUpperCase())} = document.querySelector('${item.tagName}[name=${buttonName}]')`;
     arr.push(varable);
   } else {
     let varable = `button = document.querySelector('${item.tagName}')`;
